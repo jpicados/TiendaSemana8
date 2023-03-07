e6c8e67a7c2c118b6de7900db613263a41c46715
jpicados <98858767+jpicados@users.noreply.github.com>
2023-03-06 23:04:11 -0600

    TiendaSemana8/Categoria

---
+++ Semana 8/tienda_v2semana8/src/main/java/com/tienda_v2/domain/Categoria.java
@@ -0,0 +1,29 @@
+
+package com.tienda_v2.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="Categoria")
+public class Categoria implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCategoria;
+    private String descripcion;
+    private boolean activo;
+
+    public Categoria() {
+    }
+
+    public Categoria(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+    
+    
+}
