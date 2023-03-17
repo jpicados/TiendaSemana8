1a517b9d88e47755ad9ab34a9ad39cd109ab60e0
jpicados <98858767+jpicados@users.noreply.github.com>
2023-03-17 16:26:14 -0600

    TiendaSemana8/CreditoServiceImpl

---
+++ Semana 8/tienda_v2semana8/src/main/java/com/tienda_v2/TemplateResolverConfiguration.java
@@ -0,0 +1,37 @@
+
+package com.tienda_v2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+
+@Configuration
+public class TemplateResolverConfiguration {
+    @Bean
+    public SpringResourceTemplateResolver TemplatesResolver_0(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setOrder(0);
+        templateResolver.setCheckExistence(true);
+        return templateResolver;
+    }
+    
+    @Bean
+    public ClassLoaderTemplateResolver TemplatesResolver_1(){
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("classpath:/categoria/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setOrder(1);
+        templateResolver.setCheckExistence(true);
+        return templateResolver;
+    }
+}
+
